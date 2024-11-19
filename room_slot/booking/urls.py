b7488c31b84d1608d4255608a9109638ae602c06
huytrandev <huytq1226@gmail.com>
2024-11-19 15:49:17 +0700

    sửa phần contact-us

--- room_slot/booking/urls.py
+++ room_slot/booking/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('contact', views.contact, name='contact'),
+    path('contact-us', views.contact, name='contact'),
     path('book', views.book, name='book'),
     path('book-now/<int:id>', views.book_now, name='book_now'),
     path('book_confirm', views.book_confirm, name='book_confirm'),
