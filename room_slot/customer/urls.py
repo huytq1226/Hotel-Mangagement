34a6862d1b5724b31ddb8f6c81fdfffc7b230366
huytrandev <huytq1226@gmail.com>
2024-11-29 17:30:25 +0700

    Them chuc nang doi mat khau

--- room_slot/customer/urls.py
+++ room_slot/customer/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns=[
     path('',views.dashboard,name='user_dashboard'),
-    path('details/<int:id>/<int:booking_id>',views.details,name='user_details')
+    path('details/<int:id>/<int:booking_id>',views.details,name='user_details'),
+    path('dashboard/change_password/', views.change_password, name='change_password'),
 ]
--- room_slot/customer/views.py
+++ room_slot/customer/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render,redirect
 from login.models import Customer
 from booking.models import Booking
+from django.contrib.auth.hashers import check_password
+from django.contrib import messages
+from django.contrib.auth.hashers import make_password
+
+
+
 import datetime
 def dashboard(request):
   if request.session.get('username',None) and request.session.get('type',None)=='manager':
@@ -14,6 +20,7 @@ def dashboard(request):
       return render(request,"user_dash/index.html",{"data":booking_data,"count":counts,"available":available})
   else:
       return redirect("user_login")
+  
 def details(request,id,booking_id):
     if not request.session.get('username',None):
       return redirect('manager_login')
@@ -26,3 +33,34 @@ def details(request,id,booking_id):
     except:
         return redirect("/manager/dashboard1/")
 
+
+def change_password(request):
+    if not request.session.get('username', None):
+        return redirect('user_login')
+
+    if request.method == 'POST':
+        old_password = request.POST.get('old_password')
+        new_password = request.POST.get('new_password')
+        confirm_password = request.POST.get('confirm_password')
+
+        username = request.session['username']
+        user = Customer.objects.get(username=username)
+
+        # Kiểm tra mật khẩu cũ
+        if not check_password(old_password, user.password):  # Nếu bạn dùng hash
+            messages.error(request, 'Mật khẩu cũ không chính xác.')
+            return redirect('change_password')
+
+        # Kiểm tra mật khẩu mới và xác nhận
+        if new_password != confirm_password:
+            messages.error(request, 'Mật khẩu mới và xác nhận mật khẩu không khớp.')
+            return redirect('change_password')
+
+        # Cập nhật mật khẩu mới
+        user.password = make_password(new_password)  # Dùng hash mật khẩu
+        user.save()
+
+        messages.success(request, 'Đổi mật khẩu thành công.')
+        return redirect('user_dashboard')
+
+    return render(request, 'user_dash/change_password.html')
